14e26e126cf3bb57135a582b247e51749a376d3b
kewii33 <kewiii071@gmail.com>
2024-03-07 17:12:53 +0900

    level5

--- src/store/modules/todoSlice.ts
+++
@@ -1,9 +0,0 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const todosSlice = createSlice({
-  name: 'todos',
-  initialState: {},
-  reducers: {},
-});
-
-export default todosSlice.reducer;
